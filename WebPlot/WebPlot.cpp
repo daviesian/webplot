7fb62c9f1f693f79f149c19855bc2a784e34e90a
Ian Davies <git@iandavies.org>
2013-11-17 21:14:48 +0000

    Added mongoose.

--- WebPlot/WebPlot.cpp
+++ WebPlot/WebPlot.cpp
@@ -1,13 +1,38 @@
 #include "WebPlot.h"
+#include "mongoose.h"
+
+#include <string>
 #include <iostream>
 
 using namespace std;
 using namespace WebPlotter;
 
+int mg_begin_request(mg_connection* conn) {
+	cout << "BEGIN REQUEST" << endl;
 
+	string content = "Hello world";
+
+	mg_printf(conn,
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: %d\r\n"        // Always set Content-Length
+        "\r\n"
+        "%s",
+		content.length(), content.c_str());
+
+	return 1;
+}
 
 WebPlot::WebPlot(int port) {
+	mg_callbacks callbacks = {};
 
+	callbacks.begin_request = mg_begin_request;
+
+	const char * options[] = {
+		"listening_ports", "8080",
+		NULL
+	};
+	mg_start(&callbacks, NULL, options);
 }
 
 void WebPlot::addFigure(Figure& f) {
